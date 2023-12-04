e38bdc8f51874ae5193fc0d1c9fb2e64325761a8
amaliapirva <98283758+amaliapirva@users.noreply.github.com>
2023-12-04 21:32:53 +0200

    Update proiect.c

--- proiect.c
+++ proiect.c
@@ -2,11 +2,14 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/time.h>
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <time.h>
+
+#define MAX_PATH 1024
 
 void afisareEroare(const char *mesaj) {
     perror(mesaj);
@@ -32,63 +35,143 @@ typedef struct {
     unsigned int importantColors;
 } BMPHeader;
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        afisareEroare("Usage: ./program <fisier_intrare>");
+void convertToGrayScale(const char *inputPath, const char *outputPath) {
+    int inputFile = open(inputPath, O_RDONLY);
+    if (inputFile == -1) {
+        afisareEroare("Eroare la deschiderea fișierului BMP pentru citire");
     }
 
-    char *numeFisier = argv[1];
- 
-    int fisier = open(numeFisier, O_RDONLY);
-    if (fisier == -1) {
-        afisareEroare("Eroare la deschiderea fisierului");
-    }
- 
     BMPHeader header;
-    if (read(fisier, &header, sizeof(BMPHeader)) == -1) {
-        afisareEroare("Eroare la citirea din fisier");
+    ssize_t bytesRead = read(inputFile, &header, sizeof(BMPHeader));
+    if (bytesRead != sizeof(BMPHeader)) {
+        close(inputFile);
+        afisareEroare("Eroare la citirea header-ului BMP");
+    }
+
+    int outputFile = open(outputPath, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (outputFile == -1) {
+        close(inputFile);
+        afisareEroare("Eroare la deschiderea fișierului de ieșire pentru scriere");
+    }
+
+    if (write(outputFile, &header, sizeof(BMPHeader)) != sizeof(BMPHeader)) {
+        close(inputFile);
+        close(outputFile);
+        afisareEroare("Eroare la scrierea header-ului BMP in fisierul de iesire");
+    }
+
+    for (int i = 0; i < header.height; ++i) {
+        for (int j = 0; j < header.width; ++j) {
+            unsigned char pixel[3];
+            if (read(inputFile, pixel, sizeof(pixel)) != sizeof(pixel)) {
+                close(inputFile);
+                close(outputFile);
+                afisareEroare("Eroare la citirea pixelului din fisierul BMP");
+            }
+
+            unsigned char grayValue = (unsigned char)(0.299 * pixel[2] + 0.587 * pixel[1] + 0.114 * pixel[0]);
+
+            if (write(outputFile, &grayValue, sizeof(grayValue)) != sizeof(grayValue)) {
+                close(inputFile);
+                close(outputFile);
+                afisareEroare("Eroare la scrierea pixelului in fisierul de iesire");
+            }
+        }
+    }
+
+    close(inputFile);
+    close(outputFile);
+}
+
+void processFile(const char *inputPath, const char *outputDir) {
+    int inputFile = open(inputPath, O_RDONLY);
+    if (inputFile == -1) {
+        afisareEroare("Eroare la deschiderea fisierului pentru citire");
     }
 
     struct stat statFisier;
-    if (fstat(fisier, &statFisier) == -1) {
+    if (fstat(inputFile, &statFisier) == -1) {
+        close(inputFile);
         afisareEroare("Eroare la obtinerea informatiilor despre fisier");
     }
 
-    struct tm *timeInfo;
-    time_t timpModificare = statFisier.st_mtime;
-    timeInfo = localtime(&timpModificare);
-    char dataModificare[20];
-    strftime(dataModificare, sizeof(dataModificare), "%d.%m.%Y", timeInfo);
+    char numeFisierStatistica[MAX_PATH];
+    snprintf(numeFisierStatistica, sizeof(numeFisierStatistica), "%s/%s_statistica.txt", outputDir, inputPath);
 
-    char continut[300]; 
-    sprintf(continut, "nume fisier: %s\ninaltime: %d\nlungime: %d\ndimensiune: %ld\nidentificatorul utilizatorului: %d\ntimpul ultimei modificari: %s\ncontorul de legaturi: %ld\ndrepturi de acces user: %c%c%c\ndrepturi de acces grup: %c%c%c\ndrepturi de acces altii: %c%c%c\n",
-            numeFisier,
-            header.height, header.width,
-            statFisier.st_size,
-            statFisier.st_uid,
-            dataModificare,
-            statFisier.st_nlink,
-            (statFisier.st_mode & S_IRUSR) ? 'R' : '-',
-            (statFisier.st_mode & S_IWUSR) ? 'W' : '-',
-            (statFisier.st_mode & S_IXUSR) ? 'X' : '-',
-            (statFisier.st_mode & S_IRGRP) ? 'R' : '-',
-            (statFisier.st_mode & S_IWGRP) ? 'W' : '-',
-            (statFisier.st_mode & S_IXGRP) ? 'X' : '-',
-            (statFisier.st_mode & S_IROTH) ? 'R' : '-',
-            (statFisier.st_mode & S_IWOTH) ? 'W' : '-',
-            (statFisier.st_mode & S_IXOTH) ? 'X' : '-');
-
-    int fisierStatistica = open("statistica.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-    if (fisierStatistica == -1) {
-        afisareEroare("Eroare la deschiderea fisierului statistica.txt");
+    int outputFile = open(numeFisierStatistica, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (outputFile == -1) {
+        close(inputFile);
+        afisareEroare("Eroare la deschiderea fisierului de iesire pentru scriere");
     }
 
-    if (write(fisierStatistica, continut, strlen(continut)) == -1) {
-        afisareEroare("Eroare la scrierea in fisierul statistica.txt");
+    dprintf(outputFile, "Nume fisier: %s\n", inputPath);
+    dprintf(outputFile, "Dimensiune: %ld\n", statFisier.st_size);
+
+    close(inputFile);
+    close(outputFile);
+}
+
+void processDirectory(const char *inputDir, const char *outputDir) {
+    DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir(inputDir);
+    if (dir == NULL) {
+        afisareEroare("Eroare la deschiderea directorului de intrare");
     }
 
-    close(fisier);
-    close(fisierStatistica);
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            char inputPath[MAX_PATH];
+            snprintf(inputPath, sizeof(inputPath), "%s/%s", inputDir, entry->d_name);
 
-    return 0;
+            char outputPath[MAX_PATH];
+            snprintf(outputPath, sizeof(outputPath), "%s/%s_statistica.txt", outputDir, entry->d_name);
+
+            pid_t childPid = fork();
+
+            if (childPid == 0) {
+                processFile(inputPath, outputPath);
+                exit(EXIT_SUCCESS);
+            } else if (childPid > 0) {
+                int status;
+                waitpid(childPid, &status, 0);
+
+                if (WIFEXITED(status)) {
+                    printf("S-a încheiat procesul cu PID-ul %d și codul %d\n", childPid, WEXITSTATUS(status));
+
+                    int fileStatistica = open(outputPath, O_RDONLY);
+                    if (fileStatistica == -1) {
+                        afisareEroare("Eroare la deschiderea fișierului de statistică pentru citire");
+                    }
+
+                    char buffer[10];
+                    ssize_t bytesRead = read(fileStatistica, buffer, sizeof(buffer) - 1);
+                    buffer[bytesRead] = '\0';
+                    close(fileStatistica);
+
+                    printf("Procesul cu PID-ul %d a scris %s linii in %s\n", childPid, buffer, outputPath);
+                } else {
+                    printf("Procesul cu PID-ul %d nu s-a încheiat corect\n", childPid);
+                }
+            } else {
+                afisareEroare("Eroare la fork");
+            }
+        }
+    }
+
+    closedir(dir);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        afisareEroare("Usage: ./program <dir_input> <dir_output>");
+    }
+
+    const char *inputDir = argv[1];
+    const char *outputDir = argv[2];
+
+    processDirectory(inputDir, outputDir);
+
+    return EXIT_SUCCESS;
 }
